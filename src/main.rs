0344f094188220d48e8ed01c7bfcd04fcc2fbcf3
Matheus T. dos Santos <matheus.santos@edge.ufal.br>
2021-07-18 15:45:45 -0300

    WIP - implementing lexer
    Signed-off-by: Matheus T. dos Santos <matheus.santos@edge.ufal.br>

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+mod lexer;
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("simple.nx")?;
+    let mut file_content = String::new();
+    file.read_to_string(&mut file_content)?;
+
+    let toks = lexer::tokenizer(file_content);
+    let toks = toks.unwrap();
+
+    println!("Tokens: {:#?}", toks);
+
+    Ok(())
+}
